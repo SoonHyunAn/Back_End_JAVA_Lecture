ed5cdd7bf63e5e7c29603b738dad0ea65e4fe2ab
SoonHyunAn <ash7106@gmail.com>
2024-01-17 09:17:59 +0900

    제4장. 제어문

--- src/ch04_control/Q2_연습문제2.java
+++ src/ch04_control/Q2_연습문제2.java
@@ -27,22 +27,21 @@ public class Q2_연습문제2 {
 
 	public static void main(String[] args) {
 		int score = 72;
-		String grade ="";
-		switch (score/10) {
+		String grade = "";
+		switch (score / 10) {
 		case 9:
 			grade = "A";
 			break;
 		case 8:
 			grade = "B";
 			break;
-		case 7: 
+		case 7:
 			grade = "C";
 			break;
-		default: 
+		default:
 			grade = "F";
 		}
 		System.out.println(grade + "학점");
 	}
 
 }
-
