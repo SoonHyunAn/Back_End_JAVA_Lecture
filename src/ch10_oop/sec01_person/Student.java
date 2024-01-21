81ba81774bf97122c7697e9585b75ee81a8f5a60
SoonHyunAn <ash7106@gmail.com>
2024-01-21 22:46:28 +0900

    제10장. 다형성

--- src/ch10_oop/sec01_person/Student.java
+++ src/ch10_oop/sec01_person/Student.java
@@ -6,6 +6,4 @@ public class Student extends Person{
 	public void study() {
 		System.out.println(name + "이/가 공부를 합니다.");
 	}
-	
-	
 }
--- src/ch10_oop/sec02_Constructor/Student.java
+++ src/ch10_oop/sec02_Constructor/Student.java
@@ -11,6 +11,5 @@ public class Student extends Person{
 	public void study() {
 		System.out.println(name + "이/가 공부를 합니다.");
 	}
-	
-	
+
 }
