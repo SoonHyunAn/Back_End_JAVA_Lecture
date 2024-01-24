e3b43f52dcb1e81cd0a973cab7ea478a6b311d7c
SoonHyunAn <ash7106@gmail.com>
2024-01-24 10:20:46 +0900

    제14장. 예외처리

---
+++ src/ch14_exception/Ex09_class/MyException.java
@@ -0,0 +1,16 @@
+package ch14_exception.Ex09_class;
+
+//Checked Exception 
+public class MyException extends Exception{
+
+	public MyException() {
+		
+	}
+
+	
+	public MyException(String message) {
+		super(message);
+	
+	}
+
+}
