be0d1d08791748ea2ce5c1511e3466b6645f64c9
SoonHyunAn <ash7106@gmail.com>
2024-01-30 14:49:18 +0900

    extra. OpenAPI

--- src/extra_OpenAPI/RoadAddrMain.java
+++ src/extra_OpenAPI/RoadAddrMain.java
@@ -4,6 +4,5 @@ public class RoadAddrMain {
     public static void main(String[] args) throws Exception {
         RoadAddrApi api = new RoadAddrApi();
         System.out.println(api.getRoadAddr("경기도청"));
-
     }
 }
