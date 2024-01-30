d0cfa47b42dbd4789b32f80989f4280baed72505
SoonHyunAn <ash7106@gmail.com>
2024-01-30 16:22:56 +0900

    extra. 크롤링

--- src/extra_crawiling/sec03_melon/Main.java
+++ src/extra_crawiling/sec03_melon/Main.java
@@ -1,6 +1,10 @@
 package extra_crawiling.sec03_melon;
 
+import extra_crawiling.sec04_csv.CsvUtil;
+import extra_crawiling.sec04_csv.CsvUtilImpl;
+
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
@@ -11,5 +15,19 @@ public class Main {
         for (MelonChart x : list) {
             System.out.println(x);
         }
+        CsvUtil csvUtil = new CsvUtilImpl();
+
+        List<MelonChart> list2 = melonChartService.getMelon();
+        List<List<String>> csvList = new ArrayList<>();
+        for (MelonChart ib : list) {
+            List<String> line = new ArrayList<>();
+            line.add(ib.getRank() + "\t");
+            line.add(ib.getTitle() + "\t");
+            line.add(ib.getArtist() + "\t");
+            line.add(ib.getAlbum() + "\t");
+            line.add(ib.getSrc() + "\t");
+            csvList.add(line);
+        }
+        csvUtil.writeCsv("C:/Workspace/WebProject/05. JAVA/lesson/src/extra_crawiling/sec04_csv/melon.csv", csvList, "\t");
     }
 }
