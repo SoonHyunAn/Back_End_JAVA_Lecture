f763ab6f70c36468a73895e25e9adfe70ed62f47
SoonHyunAn <ash7106@gmail.com>
2024-01-30 15:20:47 +0900

    ect.

--- src/extra_hexcode/hexcode_transfer.java
+++ src/extra_hexcode/hexcode_transfer.java
@@ -1,11 +1,9 @@
-package extra_hexcode;
-
-
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class hexcode_transfer {
-    List<> list = new ArrayList();
-    list.
-}
+//package extra_hexcode;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class hexcode_transfer {
+//    List<> list = new ArrayList();
+//    list.
+//}
