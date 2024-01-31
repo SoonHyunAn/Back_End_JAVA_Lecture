001e8ee86f9b0ffaaba1d717405888bdeb2dd70f
SoonHyunAn <ash7106@gmail.com>
2024-01-31 11:56:42 +0900

    extra. OpenAPI

--- src/extra_OpenAPI/KakaoLocalApi.java
+++ src/extra_OpenAPI/KakaoLocalApi.java
@@ -1,35 +1,64 @@
-//package extra_OpenAPI;
-//
-//import java.io.BufferedReader;
-//import java.io.FileReader;
-//import java.net.URI;
-//import java.net.URLEncoder;
-//import java.net.http.HttpHeaders;
-//import java.util.Map;
-//
-///*
-//도로명주소로부터 위도(Latitude), 경도(Longitude) 정보를 구해주는 메소드
-// */
-//public class KakaoLocalApi {
-//    public Map<String, Double> getGeocode(String addr) throws Exception {
-//        BufferedReader br = new BufferedReader(new FileReader(("C:/Workspace/WebProject/05. JAVA/lesson/src/extra_OpenAPI/kakaoApiKey.txt")));
-//        String kakaoKey = br.readLine();
-//        br.close();
-//        String query = URLEncoder.encode(addr, "utf-8");
-//        String apiUrl = "https://dapi.kakao.com/v2/local/search/address.json" + "?query=" + query;
-//
-////        URL url = new URL(apiUrl);
-////        // 헤더 설정
-////        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-////        conn.setRequestProperty("Authorization", "KakaoAK "+kakaoKey);
-////        conn.setDoInput(true);
-//
-//        URI uri = new URI(apiUrl);
-//        // 헤더 설정
-//        HttpHeaders headers= new HttpHeaders ();
-//
-//
-//
-//        return null;
-//    }
-//}
+package extra_OpenAPI;
+
+import ch09_Modifier.sec02_access.xyz.D;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+도로명주소로부터 위도(Latitude), 경도(Longitude) 정보를 구해주는 메소드
+ */
+public class KakaoLocalApi {
+    public Map<String, Double> getGeocode(String addr) throws Exception {
+        BufferedReader br = new BufferedReader(new FileReader(("C:/Workspace/WebProject/05. JAVA/lesson/src/extra_OpenAPI/kakaoApiKey.txt")));
+        String kakaoKey = br.readLine();
+        br.close();
+        String query = URLEncoder.encode(addr, "utf-8");
+        String apiUrl = "https://dapi.kakao.com/v2/local/search/address.json" + "?query=" + query;
+
+        URL url = new URL(apiUrl);
+        // 헤더 설정
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestProperty("Authorization", "KakaoAK "+kakaoKey);
+//        conn.setDoInput(true);
+
+
+        // 응답 결과 확인
+        int responseCode = conn.getResponseCode(); // 200 이 나오면 성공, 400번대면 오류
+        System.out.println(responseCode);
+
+        br = new BufferedReader(new InputStreamReader(conn.getInputStream(),"utf-8"));
+        StringBuffer sb = new StringBuffer();
+        String line = null;
+        while ((line = br.readLine()) != null)
+            sb.append(line);
+        br.close();
+//        System.out.println(sb.toString());
+
+        //Json 데이터에서 원하는 값 추출하기
+        JSONParser parser = new JSONParser();
+        JSONObject object = (JSONObject) parser.parse(sb.toString());
+//        System.out.println(object.keySet());
+        JSONArray documents = (JSONArray) object.get("documents");
+//        System.out.println(documents.size());
+        JSONObject item = (JSONObject) documents.get(0);
+//        System.out.println(item.keySet());
+        String lon_ = (String) item.get("x");
+        String lat_ = (String) item.get("y");
+//        System.out.println(lon_ + ", " + lat_);
+
+        Map<String, Double> map = new HashMap<String, Double>();
+        map.put("lon", Double.parseDouble(lon_));
+        map.put("lat", Double.parseDouble(lat_));
+        return map;
+    }
+}
