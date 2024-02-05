988f8c29d9b180cd1c585fff01dbb322dba6c77b
SoonHyunAn <ash7106@gmail.com>
2024-02-05 10:24:58 +0900

    MySQL

--- src/MySQL/sec05_Basic/Ex01_Select.java
+++ src/MySQL/sec05_Basic/Ex01_Select.java
@@ -1,6 +1,7 @@
 package MySQL.sec05_Basic;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.Statement;
 
 
@@ -9,7 +10,10 @@ public class Ex01_Select {
         Connection conn = null;
         Statement stmt = null;
         try {
-            
+            conn = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/world", // 호스트, 포트, DE명
+                    "hmuser", "hmpass"
+            );
         } catch (Exception e){
             e.printStackTrace();
         }
