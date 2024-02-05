234c11548c2ff2174488485abd34a1fe0e1d40a6
SoonHyunAn <155404398+SoonHyunAn@users.noreply.github.com>
2024-02-05 14:15:57 +0900

    MySQL

--- src/MySQL/sec06_kcity/CityDao.java
+++ src/MySQL/sec06_kcity/CityDao.java
@@ -1,10 +1,8 @@
 package MySQL.sec06_kcity;
 
 import java.io.FileInputStream;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.*;
+import java.util.List;
 import java.util.Properties;
 
 /*
@@ -49,7 +47,8 @@ public class CityDao {
     public City getCityById(int id) {
         Connection conn = myConnection();
         String sql = "select * from kcity where id = ?";
-        City city = new City();
+//        City city = new City();     // 방법 1
+        City city = null;             // 방법 2
         try {
             // 파라미터 세팅
             PreparedStatement pstmt = conn.prepareStatement(sql);
@@ -57,7 +56,9 @@ public class CityDao {
 
             // Select 실행하고 결과 받기
             ResultSet rs = pstmt.executeQuery();
-            while (rs.next()){
+            while (rs.next()) {
+                // 방법 1
+                city = new City();
                 city.setId(rs.getInt(1));
                 city.setName(rs.getString(2));
                 city.setCountryCode(rs.getString(3));
@@ -71,5 +72,50 @@ public class CityDao {
             e.printStackTrace();
         }
         return city;
+    }
+
+    public City getCityByName(String name) {
+        Connection conn = myConnection();
+        String sql = "select * from kcity where name=?";
+        City city = null; // 방법 2
+        try {
+            //  파라메터 세팅
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, name);
+
+            // Select 실행하고 결과 받기
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                city = new City(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getInt(5));
+            }
+            rs.close();
+            pstmt.close();
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return city;
+    }
+
+    public List<City> getCityListAll() {
+        Connection conn = myConnection();
+        String sql = "select * from kcity";
+        List<City> list = null;
+        try {
+            Statement stmt = conn.createStatement();
+
+            // Select 실행하고 결과 받기
+            ResultSet rs = stmt.executeQuery(sql);
+            while (rs.next()) {
+                
+            }
+            rs.close();
+            stmt.close();
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return list;
     }
 }
