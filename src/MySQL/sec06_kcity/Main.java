3b682d80a2ff3dc5ae462a2cd7264b7f8cc6aa95
SoonHyunAn <155404398+SoonHyunAn@users.noreply.github.com>
2024-02-05 14:16:19 +0900

    MySQL

--- src/MySQL/sec06_kcity/Main.java
+++ src/MySQL/sec06_kcity/Main.java
@@ -3,7 +3,9 @@ package MySQL.sec06_kcity;
 public class Main {
     public static void main(String[] args){
         CityDao cityDao = new CityDao();
-        City city = cityDao.getCityById(2340);
-        System.out.println(city);
+        City city1 = cityDao.getCityById(2340);
+        City city2 = cityDao.getCityByName("수원");
+        System.out.println(city1);
+        System.out.println(city2);
     }
 }
