af1310c3a67679f03e263cbf6d910d30adab9487
SoonHyunAn <ash7106@gmail.com>
2024-02-06 09:39:37 +0900

    MySQL

--- src/MySQL/sec04_Export, Import/01.DDL.sql
+++ src/MySQL/sec04_Export, Import/01.DDL.sql
@@ -1,25 +1,38 @@
-################################################################
+#############################
 #
 # 테이블 Export/Import
 #
-################################################################
+#############################
 
 /*
-    1. secure_file_priv 확인
-	
-	C:\ProgramData\MySQL\MySQL Server 8.0\Uploads\
-*/
+ * 1. secure_file_priv 확인
+ *
+ *          C:\ProgramData\MySQL\MySQL Server 8.0\Uploads\ - Windows
+ */
 SHOW VARIABLES LIKE 'secure_file_priv';
 
 /*
-	2. Export (Table -> File) -- backup과 유사
-*/
-SELECT * FROM song INTO Outfile
-	'C:/ProgramData/MySQL/MySQL Server 8.0/Upload/song.scv'
+ * 2. Export (Table -> File)
+ */
+SELECT * FROM song INTO OUTFILE
+	'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/song.csv'
 	FIELDS TERMINATED BY ','
 	ENCLOSED BY '"'
-	LINES TERMINATED BY '/n';
-	
+	LINES TERMINATED BY '\n';
+
 /*
-	3. Import (File -> Table) -- backup과 유사
-*/
+ * 3. Import (File -> Table)
+ */
+# 데이터 삭제
+truncate song;
+
+# 데이터 로드
+LOAD DATA INFILE
+	'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/song.csv'
+	INTO TABLE song
+	FIELDS TERMINATED BY ','
+	ENCLOSED BY '"'
+	LINES TERMINATED BY '\n';
+
+# 데이터 확인
+SELECT * FROM song;
