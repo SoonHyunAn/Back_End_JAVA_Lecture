835431b68f9e5e883375d4460aa67ce11e69e083
SoonHyunAn <ash7106@gmail.com>
2024-02-08 17:24:40 +0900

    백준

--- src/백준_연습문제/sec4_1차원_배열/level5_10810.java
+++ src/백준_연습문제/sec4_1차원_배열/level5_10810.java
@@ -6,23 +6,19 @@ public class level5_10810 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        int a = scan.nextInt();
-        int b = scan.nextInt();
-        int count = 0;
-        int[] arr = new int[100];
-        for (int i = 1; i < a; i++) {
+        int n = scan.nextInt();
+        int m = scan.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < m; i++) {
             int c = scan.nextInt();
             int d = scan.nextInt();
             int e = scan.nextInt();
-            for (int k = c-1; k <= d-1; k++) {
-                if (arr[k] != 0) count--;
-                if (count == b) break;
+            for (int k = c-1; k <= d-1; k++)
                 arr[k] = e;
-                count++;
-            }
         }
-        for (int j = 0; j < a; j++) {
-            if (j == a - 1) {
+        for (int j = 0; j < arr.length; j++) {
+            if (j == n - 1) {
                 System.out.print(arr[j]);
                 break;
             }
